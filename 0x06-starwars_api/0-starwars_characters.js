3a76711710f59fb3bc9f54f37921ffdbbfd43e96
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-05-23 21:24:25 +0100

    '0-starwars_characters.js'

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const req = (array, i) => {
+	if (i === array.length) return;
+	request(array[i], (err, res, body) => {
+		if (err) throw err;
+		else {
+			console.log(JSON.parse(body).name);
+			req(array, i + 1);
+		}
+	});
+};
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, res, body) => {
+	if (err) throw err;
+	const characters = JSON.parse(body).characters;
+	req(characters, 0);
+});
