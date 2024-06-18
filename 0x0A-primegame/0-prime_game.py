4d1659682094ef99198aad14c81b8d4ce4fe9ed4
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-06-18 15:17:06 +0100

    '0-prime_game.py'

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Prime Game"""
+
+
+def is_prime(n):
+    """Check if a number is prime"""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
