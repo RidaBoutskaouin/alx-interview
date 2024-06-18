1b90b3dfcea4ccc839770b71bcf2de513437c8ae
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-06-18 15:19:44 +0100

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-"""Prime Game"""
+"""0. Prime Game Maria and Ben are playing a game."""
 
 
-def is_prime(n):
-    """Check if a number is prime"""
-    if n < 2:
-        return False
+def isWinner(x, nums):
+    """isWinner function to determine the winner of the game"""
+    if not nums or x < 1:
+        return None
+
+    n = max(nums)
+    sieve = [1 for _ in range(max(n + 1, 2))]
+    sieve[0] = 0
+    sieve[1] = 0
     for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = 0
+
+    sieve = [i for i, n in enumerate(sieve) if n]
+    sieve = sieve[1:]
+
+    count = 0
+    for i in nums:
+        count += sum(1 for prime in sieve if prime <= i)
+    if count % 2 == 0:
+        return "Ben"
+    return "Maria"
+
